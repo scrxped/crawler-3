5367311a4ef3cfb315448f39e7530424a69006af
zstate <zstate@gmail.com>
2018-04-21 14:50:13 -0400

    Removed config from BeforeEngineStarted event because the config is passed to the extension during the initialization.

--- src/Event/BeforeEngineStarted.php
+++ src/Event/BeforeEngineStarted.php
@@ -7,21 +7,4 @@ use Zstate\Crawler\Config\Config;
 
 class BeforeEngineStarted extends Event
 {
-    /**
-     * @var Config
-     */
-    private $config;
-
-    public function __construct(Config $config)
-    {
-        $this->config = $config;
-    }
-
-    /**
-     * @return Config
-     */
-    public function getConfig(): Config
-    {
-        return $this->config;
-    }
 }
